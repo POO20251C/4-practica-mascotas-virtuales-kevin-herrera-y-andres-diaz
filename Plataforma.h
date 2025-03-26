dda04ce58ec6bb8129381949dcef15c58674bdea
AndresDiaz03 <86079407+AndresDiaz03@users.noreply.github.com>
2025-03-26 14:07:48 -0500

    Este código define la clase Plataforma.
    Que maneja usuarios, mascotas y objetos, permitiendo registrar usuarios, adoptar mascotas, darles objetos, enseñarles habilidades y consultar su historial.

---
+++ Plataforma.h
@@ -0,0 +1,27 @@
+#ifndef PLATAFORMA_H
+#define PLATAFORMA_H
+
+#include <map>
+#include <vector>
+#include "Usuario.h"
+#include "Objeto.h"
+#include "Habilidad.h"
+
+class Plataforma {
+private:
+    std::map<int, Usuario*> usuarios;
+    std::vector<Objeto> objetos;
+public:
+    Plataforma();
+    ~Plataforma();
+    void inicializarObjetos();
+    void registrarUsuario(Usuario* usuario);
+    Usuario* buscarUsuario(int id);
+    void adoptarMascotas(int idUsuario, Mascota* mascota);
+    void darObjeto(int idUsuario, const std::string &nombreMascota, const Objeto &obj);
+    void ensenarHabilidad(int idUsuario, const std::string &nombreMascota, Habilidad hab);
+    void aumentarExperiencia(int idUsuario, const std::string &nombreMascota, int exp);
+    void consultarHistorial(int idUsuario, const std::string &nombreMascota);
+};
+
+#endif //PLATAFORMA_H
